f939c6053cd666aca2d665eeb07019b42cf28eb2
viktorstrate <viktorstrate@gmail.com>
2025-05-30 11:40:25 +0200

    Move extension to its own repo

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tempo for Visual Studio Code
+
+This extension adds language support for the [Tempo](https://github.com/tempo-lang/tempo) choreographic programming language.
+
+## Features
+
+- Syntax highlighting
+- Error diagnostics
+- Code completion
+- Hover the mouse over symbols for type information
+
+## Requirements
+
+The `tempo` compiler must be installed and accessible in the `PATH` environment.
+To install Tempo, run the following command.
+
+```sh
+$ go install github.com/tempo-lang/tempo@latest
+```
---
+++ src/extension.ts
@@ -0,0 +1,58 @@
+import path = require("path");
+import { workspace, ExtensionContext } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+  // const serverCommand = context.asAbsolutePath("server");
+
+  const goPath = path.join(context.extensionUri.path, "..");
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  let serverOptions: ServerOptions = {
+    run: {
+      command: "tempo",
+      args: ["lsp"],
+    },
+    debug: {
+      command: "go",
+      args: ["run", "./main.go", "lsp"],
+      options: { cwd: goPath },
+    },
+  };
+
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: "file", language: "tempo" }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.tempo"),
+    },
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "tempoLS",
+    "Tempo Language Server",
+    serverOptions,
+    clientOptions
+  );
+
+  // Start the client. This will also launch the server
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
